6531c54ff45b36ab1b8dc0ade67184484067b4dd
Jesse <jesse-jjs@hotmail.com>
2022-07-27 20:54:21 -0300

    feat: add contact and about us controllers

--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return 'Olá, seja bem-vindo ao curso!';
-});
+Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal']);
 
-Route::get('/sobre-nos', function() {
-    return 'Sobre nós';
-});
+Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class, 'sobreNos']);
 
-Route::get('/contato', function() {
-    return 'Contato';
-});
+Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato']);
