6296be9cc6235d67bb9d2e2d148d9c32837380f1
Jesse <jesse-jjs@hotmail.com>
2022-07-27 20:45:07 -0300

    feat: add initial routes

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Olá, seja bem-vindo ao curso!';
+});
+
+Route::get('/sobre-nos', function() {
+    return 'Sobre nós';
+});
+
+Route::get('/contato', function() {
+    return 'Contato';
 });
