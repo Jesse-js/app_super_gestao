00d896b6393681ee040177a57323da4ad2ea19f4
Jesse <jesse-jjs@hotmail.com>
2022-07-27 21:56:32 -0300

    refactor: add name in routes

--- resources/views/site/sobre-nos.blade.php
+++ resources/views/site/sobre-nos.blade.php
@@ -1,12 +1,12 @@
 <h3>Sobre Nós!</h3>
 <ul>
     <li>
-        <a href="/">Principal</a>
+        <a href="{{route('site.index')}}">Principal</a>
     </li>
     <li>
-        <a href="/sobre-nos">Sobre Nós</a>
+        <a href="{{route('site.sobrenos')}}">Sobre Nós</a>
     </li>
     <li>
-        <a href="/contato">Contato</a>
+        <a href="{{route('site.contato')}}">Contato</a>
     </li>
 </ul>
--- routes/web.php
+++ routes/web.php
@@ -2,24 +2,26 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal']);
+Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal'])->name('site.index');
 
-Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class, 'sobreNos']);
+Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class, 'sobreNos'])->name('site.sobrenos');
 
-Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato']);
+Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato'])->name('site.contato');
 
-Route::get('/login', function () {
-    return 'Login';
-});
+Route::prefix('/app')->group(function () {
+    Route::get('/login', function () {
+        return 'Login';
+    })->name('app.login');
 
-Route::get('/clientes', function () {
-    return 'Clientes';
-});
+    Route::get('/clientes', function () {
+        return 'Clientes';
+    })->name('app.clientes');
 
-Route::get('/fornecedores', function () {
-    return 'Fornecedores';
-});
+    Route::get('/fornecedores', function () {
+        return 'Fornecedores';
+    })->name('app.fornecedores');
 
-Route::get('/contato', function () {
-    return 'Contato';
+    Route::get('/contato', function () {
+        return 'Contato';
+    })->name('app.contato');
 });
