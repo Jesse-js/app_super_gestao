2fbe4f824ce9c27f04b60a844007e35af4a0789f
Jesse <jesse-jjs@hotmail.com>
2022-07-27 21:41:50 -0300

    feat: add new routes

--- routes/web.php
+++ routes/web.php
@@ -2,19 +2,24 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal']);
 
 Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class, 'sobreNos']);
 
 Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato']);
+
+Route::get('/login', function () {
+    return 'Login';
+});
+
+Route::get('/clientes', function () {
+    return 'Clientes';
+});
+
+Route::get('/fornecedores', function () {
+    return 'Fornecedores';
+});
+
+Route::get('/contato', function () {
+    return 'Contato';
+});
