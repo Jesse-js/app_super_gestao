6998437a0b9f3921b5f364ad1613b6ed3e05cc21
Jesse-js <jesse-jjs@hotmail.com>
2023-07-25 13:47:54 -0300

    feat: creation of controllers

---
+++ app/Http/Controllers/ContatoController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ContatoController extends Controller
+{
+    //
+    public function contato(){
+
+        var_dump($_POST);
+        return view('site.contato', ['titulo' => 'Contato']);
+    }
+}
