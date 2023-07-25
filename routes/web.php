f13df924870c602b854545be7026b050b835edaa
Jesse-js <jesse-jjs@hotmail.com>
2023-07-25 13:45:07 -0300

    chore: project structure

---
+++ routes/web.php
@@ -0,0 +1,59 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+/*
+Route::get('/', function () {
+    return 'Olá, seja bem-vindo ao curso!';
+});
+*/
+
+Route::get('/', 'PrincipalController@principal')->name('site.index');
+Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
+Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+Route::post('/contato', 'ContatoController@contato')->name('site.contato');
+Route::get('/login', function() { return 'Login'; })->name('site.login');
+
+Route::prefix('/app')->group(function() {
+    Route::get('/clientes', function() { return 'Clientes'; })->name('app.clientes');
+    Route::get('/fornecedores', 'FornecedorController@index')->name('app.fornecedores');
+    Route::get('/produtos', function() { return 'Proditos'; })->name('app.produtos');
+});
+
+
+Route::get('rota1', function() {
+    echo 'Rota 1';
+})->name('site.rota1');
+
+Route::get('/rota2', function() {
+    return redirect()->route('site.rota1');
+})->name('site.rota2');
+
+Route::get('/teste/{p1}/{p2}', 'TesteController@teste')->name('site.teste');
+
+Route::fallback(function() {
+    echo 'A rota acessada não existe. <a href="'.route('site.index').'">Clique aqui</a> para ir para a página inicial';
+});
+
+//Route::redirect('/rota2', '/rota1');
+
+/*
+Route::get('/contato/{nome}/{categoria_id}',
+    function(
+            string $nome = 'Jessé',
+            int $categoria = 50
+    ) {
+        echo 'Estamos aqui! '.$nome.' '.$categoria;
+    }
+)->where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]+');
+*/
