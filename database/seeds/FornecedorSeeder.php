d70771963972acd610ea3099bd2c47478227e14b
Jesse-js <jesse-jjs@hotmail.com>
2023-07-29 16:00:04 -0300

    feat: adding suppliers seeder

---
+++ database/seeds/FornecedorSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Fornecedor;
+class FornecedorSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $fornecedor = new Fornecedor();
+        $fornecedor->nome = 'Fornecedor 100';
+        $fornecedor->site = 'fornecedor100.com.br';
+        $fornecedor->uf = 'CE';
+        $fornecedor->email = 'contato@fornecedor100.com.br';
+        $fornecedor->save();
+
+        Fornecedor::create([
+            'nome' => 'Fornecedor 200',
+            'site' => 'forcedor200.com.br',
+            'uf' => 'RS',
+            'email' => 'contato@fornecedor200.com.br'
+        ]);
+
+        DB::table('fornecedores')->insert([
+            'nome' => 'Fornecedor 300',
+            'site' => 'forcedor300.com.br',
+            'uf' => 'SP',
+            'email' => 'contato@fornecedor300.com.br' 
+        ]);
+    }
+}
