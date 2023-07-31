943ed7a07d73e59f72d5b12f89107e6d330b08af
Jesse-js <jesse-jjs@hotmail.com>
2023-07-31 19:51:16 -0300

    feat: saving site contact registers

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -3,13 +3,34 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\SiteContato;
 
 class ContatoController extends Controller
 {
     //
-    public function contato(){
+    public function contato(Request $request){
+        /*echo '<pre>';
+        print_r($request->all());
+        echo '</pre>';
+        echo $request->input('nome');
+        echo '<br>';
+        echo $request->input('email');*/
+        /*$contato = new SiteContato();
+        $contato->nome = $request->input('nome');
+        $contato->telefone = $request->input('telefone');
+        $contato->email = $request->input('email');
+        $contato->motivo_contato = $request->input('motivo_contato');
+        $contato->mensagem = $request->input('mensagem');
 
-        var_dump($_POST);
+        $contato->save();*/
+
+        /*$contato = new SiteContato();
+        $contato->fill($request->all());
+        print_r($contato->getAttributes());
+        $contato->save();*/
+
+        $contato = new SiteContato();
+        $contato->create($request->all());
         return view('site.contato', ['titulo' => 'Contato']);
     }
 }
--- app/SiteContato.php
+++ app/SiteContato.php
@@ -7,4 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class SiteContato extends Model
 {
     //
+    protected $fillable = ['nome', 'telefone', 'email', 'motivo_contato', 'mensagem'];
 }
