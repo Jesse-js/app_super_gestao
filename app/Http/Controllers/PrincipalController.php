58b6243da8c4f9db4855b0929d1260912ffda8e0
Jesse-js <jesse-jjs@hotmail.com>
2023-08-01 09:29:10 -0300

    fix: adjust main controller

--- app/Http/Controllers/PrincipalController.php
+++ app/Http/Controllers/PrincipalController.php
@@ -8,6 +8,11 @@ class PrincipalController extends Controller
 {
     //
     public function principal() {
-        return view('site.principal', ['titulo' => 'Home']);
+        $motivo_contatos = [
+            '1' => 'Dúvida',
+            '2' => 'Elogio',
+            '3' => 'Reclamação',
+        ];
+        return view('site.principal', ['titulo' => 'Home', 'motivo_contatos' => $motivo_contatos]);
     }
 }
