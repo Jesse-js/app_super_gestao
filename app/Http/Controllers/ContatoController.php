1353da6ffdc48112c52f35b82968ae13e5525320
Jesse-js <jesse-jjs@hotmail.com>
2023-08-01 09:14:50 -0300

    feat: field fit with validation

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -31,7 +31,12 @@ class ContatoController extends Controller
 
         /*$contato = new SiteContato();
         $contato->create($request->all());*/
-        return view('site.contato', ['titulo' => 'Contato']);
+        $motivo_contatos = [
+            '1' => 'Dúvida',
+            '2' => 'Elogio',
+            '3' => 'Reclamação',
+        ];
+        return view('site.contato', ['titulo' => 'Contato', 'motivo_contatos' => $motivo_contatos]);
     }
 
     public function salvar(Request $request) 
--- resources/views/site/layouts/_components/form_contato.blade.php
+++ resources/views/site/layouts/_components/form_contato.blade.php
@@ -10,18 +10,13 @@
     <br>
     <select name="motivo_contato" class="{{ $classe }}">
         <option value="">Qual o motivo do contato?</option>
-        <option value="1">Dúvida</option>
-        <option value="2">Elogio</option>
-        <option value="3">Reclamação</option>
+        @foreach ($motivo_contatos as $item => $motivo_contato)
+            <option value="{{ $item }}" {{ old('motivo_contato') == $item ? 'selected' : '' }}>{{ $motivo_contato }}</option>
+        @endforeach
     </select>
     <br>
     <textarea name="mensagem" class="{{ $classe }}">
-        @if(old('mensagem') != '')
-            {{old('mensagem')}}
-        @else
-            Preencha aqui a sua mensagem
-        @endif
-    </textarea>
+        {{ old('mensagem') != '' ? old('mensagem') : 'Preencha aqui a sua mensagem' }}</textarea>
     <br>
     <button type="submit" class="{{ $classe }}">ENVIAR</button>
 </form>
