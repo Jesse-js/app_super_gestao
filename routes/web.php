48a3d24c4a74066089f735505ee4a111869fc788
Jesse-js <jesse-jjs@hotmail.com>
2023-08-21 11:35:20 -0300

    feat: adding app controllers

--- routes/web.php
+++ routes/web.php
@@ -27,14 +27,11 @@ Route::get('/login/{erro?}', 'LoginController@index')->name('site.login');
 Route::post('/login', 'LoginController@autenticar')->name('site.login');
 
 Route::middleware('autenticacao:ldap,visitante')->prefix('/app')->group(function () {
-    Route::get('/cliente', function () {
-        return 'Clientes';
-    })->name('app.cliente');
-
+    Route::get('/cliente', 'ClienteController@index')->name('app.cliente');
+    Route::get('/home', 'HomeController@index')->name('app.home');
+    Route::get('/sair', 'LoginController@sair')->name('app.sair');
     Route::get('/fornecedor', 'FornecedorController@index')->name('app.fornecedor');
-    Route::get('/produto', function () {
-        return 'Produtos';
-    })->name('app.produto');
+    Route::get('/produto', 'ProdutoController@index')->name('app.produto');
 });
 
 
