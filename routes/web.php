da05f435faecb2518dc54098d304aa02ff6d7699
Jesse-js <jesse-jjs@hotmail.com>
2023-08-21 11:31:28 -0300

    refactor: adjust route names

--- routes/web.php
+++ routes/web.php
@@ -27,13 +27,14 @@ Route::get('/login/{erro?}', 'LoginController@index')->name('site.login');
 Route::post('/login', 'LoginController@autenticar')->name('site.login');
 
 Route::middleware('autenticacao:ldap,visitante')->prefix('/app')->group(function () {
-    Route::get('/clientes', function () {
+    Route::get('/cliente', function () {
         return 'Clientes';
-    })->name('app.clientes');
-    Route::get('/fornecedores', 'FornecedorController@index')->name('app.fornecedores');
-    Route::get('/produtos', function () {
+    })->name('app.cliente');
+
+    Route::get('/fornecedor', 'FornecedorController@index')->name('app.fornecedor');
+    Route::get('/produto', function () {
         return 'Produtos';
-    })->name('app.produtos');
+    })->name('app.produto');
 });
 
 
