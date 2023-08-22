877cf5448038e7d3d908dbe7ad435345fe0892bb
Jesse-js <jesse-jjs@hotmail.com>
2023-08-22 14:39:21 -0300

    feat: create suppliers method

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Fornecedor;
 use Illuminate\Http\Request;
 
 class FornecedorController extends Controller
@@ -12,13 +13,38 @@ class FornecedorController extends Controller
         return view('app.fornecedor.index');
     }
 
-    public function listar()
+    public function listar(Request $request)
     {
         return view('app.fornecedor.listar');
     }
 
-    public function adicionar()
+    public function adicionar(Request $request)
     {
-        return view('app.fornecedor.adicionar');
+        $msg = '';
+        if ($request->input('_token') != '') {
+            $regras = [
+                'nome' => 'required|min:3|max:40',
+                'site' => 'required',
+                'uf' => 'required|min:2|max:2',
+                'email' => 'email',
+            ];
+
+            $feedback = [
+                'required' => 'O campo :attribute deve ser preenchido!',
+                'nome.min' => 'O campo nome deve ter no mínimo 3 caracteres',
+                'nome.max' => 'O campo nome deve ter no máximo 40 caracteres',
+                'uf.min' => 'O campo uf deve ter no mínimo 2 caracteres',
+                'uf.max' => 'O campo uf deve ter no máixmo 2 caracteres',
+                'email.email' => 'O campo e-mail não foi preenchido corretamente'
+            ];
+
+            $request->validate($regras, $feedback);
+
+            $fornecedor = new Fornecedor();
+            $fornecedor->create($request->all());
+
+            $msg = 'Caadstro realizado com sucesso';
+        }
+        return view('app.fornecedor.adicionar', ['msg' => $msg]);
     }
 }
--- resources/views/app/fornecedor/adicionar.blade.php
+++ resources/views/app/fornecedor/adicionar.blade.php
@@ -12,12 +12,18 @@
             </ul>
         </div>
         <div class="informacao-pagina">
+            {{ $msg }}
             <div style="width: 30%; margin-left:auto; margin-right:auto;">
-                <form action="" method="post">
-                    <input type="text" name="nome" placeholder="Nome" class="borda-preta">
-                    <input type="text" name="site" placeholder="Site" class="borda-preta">
-                    <input type="text" name="uf" placeholder="UF" class="borda-preta">
-                    <input type="text" name="email" placeholder="E-mail" class="borda-preta">
+                <form action="{{ route('app.fornecedor.adicionar') }}" method="post">
+                    @csrf
+                    <input type="text" name="nome" value="{{ old('nome') }}" placeholder="Nome" class="borda-preta">
+                    {{ $errors->has('nome') ? $errors->first('nome') : '' }}
+                    <input type="text" name="site" value="{{ old('site') }}" placeholder="Site" class="borda-preta">
+                    {{ $errors->has('site') ? $errors->first('site') : '' }}
+                    <input type="text" name="uf" value="{{ old('uf') }}" placeholder="UF" class="borda-preta">
+                    {{ $errors->has('uf') ? $errors->first('uf') : '' }}
+                    <input type="text" name="email" value="{{ old('email') }}" placeholder="E-mail" class="borda-preta">
+                    {{ $errors->has('email') ? $errors->first('email') : '' }}
                     <button type="submit" class="borda-preta">Cadastrar</button>
                 </form>
             </div>
