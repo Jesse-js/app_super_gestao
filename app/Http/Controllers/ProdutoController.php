fa8429cc94c5e25be9c32b8b15573d3d6fbd140f
Jesse-js <jesse-jjs@hotmail.com>
2023-08-26 18:40:30 -0300

    refactor: adjusting product details with eloquent

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -17,23 +17,6 @@ class ProdutoController extends Controller
     public function index(Request $request)
     {
         $produtos = Produto::paginate(10);
-
-        foreach($produtos as $key => $produto){
-            print_r($produto->getAttributes());
-            echo '<br><br>';
-
-            $produtoDetalhe = ProdutoDetalhe::where('produto_id', $produto->id)->first();
-
-            if(isset($produtoDetalhe)){
-                print_r($produtoDetalhe->getAttributes());
-                
-                $produtos[$key]['comprimento'] = $produtoDetalhe->comprimento;
-                $produtos[$key]['largura'] = $produtoDetalhe->largura;
-                $produtos[$key]['altura'] = $produtoDetalhe->altura;
-            }
-            echo '<hr>';
-        }
-
         return view('app.produto.index', ['produtos' => $produtos, 'request' => $request->all() ]);
     }
 
