bfcc31627c121acdcd6190629db9f35b65677453
Jesse-js <jesse-jjs@hotmail.com>
2023-08-26 21:39:36 -0300

    feat: add relation between suppliers and product

---
+++ database/migrations/2023_08_27_001951_alter_produtos_relacionamento_fornecedores.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterProdutosRelacionamentoFornecedores extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('produtos', function(Blueprint $table){
+
+            $fornecedorId = DB::table('fornecedores')->insertGetId([
+                'nome' => 'Fornecedor Padrão SG',
+                'site' => 'fornecedorpadraosg.com.br',
+                'uf' => 'SP',
+                'email' => 'contato@fornecedorpadraosg.com.br',
+            ]);
+            $table->unsignedBigInteger('fornecedor_id')->default($fornecedorId)->after('id');
+            $table->foreign('fornecedor_id')->references('id')->on('fornecedores');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('produtos', function(Blueprint $table){
+            $table->dropForeign('produtos_fornecedor_id_foreign');
+            $table->dropColumn('fornecedor_id');
+        });
+    }
+}
